bdbef7c958ee8dd1889c533f999b73ca0df46b6b
iliapolo <Eli.polonsky@gmail.com>
2013-02-19 13:02:12 +0200

    CLOUDIFY-1526 new interface to expose remote storage api calls

---
+++ dsl/src/main/java/org/cloudifysource/dsl/internal/context/RemoteStorageProvisioningDriver.java
@@ -0,0 +1,17 @@
+package org.cloudifysource.dsl.internal.context;
+
+import java.rmi.Remote;
+
+import org.cloudifysource.dsl.context.ServiceContext;
+
+/**
+ * This interface exposes Storage API methods that can be executed over LRMI from the USM to the ESM.
+ * servers as an entry point for services that wish to use the storage functionality at runtime via the {@link ServiceContext}
+ * @author elip
+ *
+ */
+public interface RemoteStorageProvisioningDriver extends Remote {
+	
+	String ping();
+
+}
